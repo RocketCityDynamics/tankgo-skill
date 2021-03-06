ef797e55397f3c5806b6fdc0502cbf34f7531ba6
Rocket City Dynamics <46632364+RocketCityDynamics@users.noreply.github.com>
2021-03-06 16:58:23 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -39,15 +39,15 @@ class Tankgo(MycroftSkill):
         while counter < 50000:
             counter += 1        
             
-            GPIO.output(in1,GPIO.HIGH)
-            GPIO.output(in2,GPIO.LOW)
-            GPIO.output(in3,GPIO.LOW)
-            GPIO.output(in4,GPIO.HIGH)
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
         
             #tank forward
             #Motor power setup here. Just one speed.
-            p.start(50)
-            p2.start(55) #l motor is a little weaker on my setup.
+        p.start(50)
+        p2.start(55) #l motor is a little weaker on my setup.
             #Compensate with slightly more juice going to the weaker motor to help it drive straighter.
         
         
